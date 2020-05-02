4d9e8870aa8c393ee06d9c221087e1ae642c577c
Dante J. Anjos <dante.anjos@MBP-de-Dante.home>
2020-05-02 20:11:11 -0300

    Telas básicas para iniciar desenvolvimento + bottom navbar

---
+++ mobile/src/pages/Home/index.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+// import { Feather } from '@expo/vector-icons';
+// import { useNavigation } from '@react-navigation/native';
+// import formatMoney from '../../utils/formatMoney';
+// import api from '../../services/api';
+
+// import Logo from '../../assets/logo.png';
+import styles from './styles';
+
+export default function Home() {
+  // const [incidents, setIncidents] = useState([]);
+  // const [total, setTotal] = useState(0);
+  // const [page, setPage] = useState(1);
+  // const [loading, setLoading] = useState(false);
+
+  // const navigation = useNavigation();
+
+  // function navigateToDetail(incident) {
+  //   navigation.navigate('Detail', { incident });
+  // }
+
+  // async function loadIncidents() {
+  //   if (loading) {
+  //     return;
+  //   }
+
+  //   if (total > 0 && incidents.length === total) {
+  //     return;
+  //   }
+
+  //   setLoading(true);
+
+  //   const response = await api.get('/incidents', {
+  //     params: { page }
+  //   });
+
+  //   setTotal(response.headers['x-total-count']);
+  //   setIncidents([...incidents, ...response.data]);
+  //   setPage(page + 1);
+  //   setLoading(false);
+  // }
+
+  // useEffect(() => {
+  //   loadIncidents();
+  // }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  )
+}
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,19 +1,87 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const AppStack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
 
-import Incidents from './pages/Incidents';
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={Home} />
+//       <Tab.Screen name="Detail" component={Detail} />
+//     </Tab.Navigator>
+//   );
+// }
+// const AppStack = createStackNavigator();
+
+import Home from './pages/Home';
 import Detail from './pages/Detail';
 
-export default function Routes() {
+// export default function Routes() {
+//   return (
+//     <NavigationContainer>
+//       <AppStack.Navigator screenOptions={{ headerShown: false }}>
+//         <AppStack.Screen name="Home" component={Home} />
+//         <AppStack.Screen name="Detail" component={Detail} />
+//       </AppStack.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Ionicons } from '@expo/vector-icons';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+// function SettingsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <AppStack.Navigator screenOptions={{ headerShown: false }}>
-        <AppStack.Screen name="Incidents" component={Incidents} />
-        <AppStack.Screen name="Detail" component={Detail} />
-      </AppStack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Detail') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" title="ddd" component={Home} />
+        <Tab.Screen name="Detail" component={Detail} />
+      </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
