a6b435f541ef8414fdb752e5052c6cd4394164b7
Dante J. Anjos <dante.anjos@MBP-de-Dante.home>
2020-05-03 03:25:53 -0300

    Removed backend and web folders

--- backend/tests/unit/generateUid.spec.js
+++
@@ -1,9 +0,0 @@
-const generateUid = require('../../src/utils/generateUid');
-
-describe('Generate Unique ID', () => {
-  it('should generate an unique id', () => {
-    const id = generateUid();
-
-    expect(id).toHaveLength(8);
-  });
-});
