2b1717d43b6aa90610d2295cb530de14f18bb489
Dante J. Anjos <dante.anjos@MBP-de-Dante.home>
2020-05-04 03:22:53 -0300

    Workaround to prevent warnings

--- mobile/App.js
+++ mobile/App.js
@@ -3,6 +3,9 @@ import Routes from './src/routes';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Setting a timer']);
+
 export default function App() {
   return (
     <Routes />
